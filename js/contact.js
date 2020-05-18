4e86def9021ca7f1284175d4f144853dc162cad4
Laris Mahmutaj <laris.mahmutaj@gmail.com>
2020-05-19 01:43:49 +0200

    Layout Fixes
    Co-authored-by: Arbnora Reshani <arbnorareshani@gmail.com>

--- js/contact.js
+++ js/contact.js
@@ -1,51 +1,35 @@
-function formValidation(){
-    var name = document.getElementById("name").value;
-    var number = document.getElementById("phoneNumber").value;
-    var email = document.getElementById("emailAddress").value;
-    var message = document.getElementById("message").value;
-    var error_msg = document.getElementById("error_message");   
-    var text;
-    
-    error_msg.style.padding = "10px";
+function formValidation() {
+  var name = document.getElementById("name")
+  var email = document.getElementById("emailAddress")
+  var message = document.getElementById("message")
 
-    if(name == ''){
-        text="Please enter your name";
-        error_msg.innerHTML = text;
-        return false;
-    }
-    else if(name.length < 5){
-        text="Please Enter Valid Name";
-        error_msg.innerHTML = text;
-        return false;
-    }
-    else if(number == ''){
-        text="Please enter your phone number";
-        error_msg.innerHTML = text;
-        return false;
-    }
-    else if(number.length < 9){
-        text="Please Enter Valid Phone Number";
-        error_msg.innerHTML = text;
-        return false;
-    }
-    else if(email == ''){
-        text="Please enter your email";
-        error_msg.innerHTML = text;
-        return false;
-    }
-    else if(!email.includes('@') || !email.includes('.')){
-        text = "Please Email must contain '@' and ' . '";
-        error_msg.innerHTML = text;
-        return false;
-    }
-    else if(message == ''){
-        text="Please enter your message";
-        error_msg.innerHTML = text;
-        return false;
-    }
-    else{
+  var nameError = document.getElementById("name-error")
+  var emailError = document.getElementById("email-error")
+  var messageError = document.getElementById("message-error")
 
-        return true;
-    }
+  var valid = true
 
+  if (name.value == "") {
+    nameError.innerHTML = "Name is a required field"
+    name.style.border = "2px solid red"
+    valid = false
+  }
+
+  if (email.value == "") {
+    emailError.innerHTML = "Email is a required field"
+    email.style.border = "2px solid red"
+    valid = false
+  } else if (!email.value.includes("@") || !email.value.includes(".")) {
+    emailError.innerHTML = "Please enter a valid email"
+    email.style.border = "2px solid red"
+    valid = false
+  }
+
+  if (message.value == "") {
+    messageError.innerHTML = "Message is a required field"
+    message.style.border = "2px solid red"
+    valid = false
+  }
+
+  return valid
 }
