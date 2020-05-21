81ddfcdc89c2d49b35cf502463141246a12a62ac
Laris Mahmutaj <laris.mahmutaj@gmail.com>
2020-05-21 21:01:40 +0200

    Error message color change

--- js/contact.js
+++ js/contact.js
@@ -7,27 +7,33 @@ function formValidation() {
   var emailError = document.getElementById("email-error")
   var messageError = document.getElementById("message-error")
 
+  var re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+
+  var emailRegex = new RegExp(re)
+
+  var errorColor = "#EA2D2E"
+
   var valid = true
 
   if (name.value == "") {
     nameError.innerHTML = "Name is a required field"
-    name.style.border = "2px solid red"
+    name.style.border = `3px solid ${errorColor}`
     valid = false
   }
 
   if (email.value == "") {
     emailError.innerHTML = "Email is a required field"
-    email.style.border = "2px solid red"
+    email.style.border = `3px solid ${errorColor}`
     valid = false
-  } else if (!email.value.includes("@") || !email.value.includes(".")) {
+  } else if (!emailRegex.test(email.value)) {
     emailError.innerHTML = "Please enter a valid email"
-    email.style.border = "2px solid red"
+    email.style.border = `3px solid ${errorColor}`
     valid = false
   }
 
   if (message.value == "") {
     messageError.innerHTML = "Message is a required field"
-    message.style.border = "2px solid red"
+    message.style.border = `3px solid ${errorColor}`
     valid = false
   }
 
